373ad031f0fe5ffe5f3c3ed6143e7647df8ee128
Rakagiffari <rakagiffari945@gmail.com>
2026-08-20 22:56:47 +0700

    Create Tentang.py

---
+++ pages/Tentang.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("📘 About Research")
+
+st.markdown("""
+
+## Judul Penelitian
+
+Penerapan Algoritma Random Forest untuk Mengklasifikasi Layanan Servis pada Yamaha.
+
+---
+
+## Algoritma
+
+- Random Forest Classifier
+
+---
+
+## Tools
+
+- Python
+- Streamlit
+- Scikit-learn
+- Plotly
+
+""")
